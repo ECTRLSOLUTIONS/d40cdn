13057276b37d074a9c6479255746237920ef6c96
dariocavada <dario.cavada.lab@gmail.com>
2021-04-13 10:28:36 +0200

    SuggestoMap - Multi Instance

--- map/libs/suggestomap.js
+++ map/libs/suggestomap.js
@@ -29,62 +29,81 @@ var SuggestoMapLayers = [
 	Thunderforest_Atlas*/
 ]
 
-var SuggestoMap = {
-	lmap: null,
-	mlist: [],
-	createMap: function (jsonData) {
-		this.lmap = L.map('mapid', { layers: SuggestoMapLayers }).setView(jsonData.mapcenter, jsonData.zoom);
-		//OpenStreetMapLayer.addTo(this.lmap);
+function SuggestoMap(mapid) {
+	this.sm = { 
+		lmap: null,
+		tileLayer: null,
+		mlist: [],
+		createMap: function (jsonData) {
+			this.lmap = L.map(mapid).setView(jsonData.mapcenter, jsonData.zoom);
+			
+			if (jsonData.tilelayer == 'Esri_WorldImagery') {
+				this.tileLayer = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+					attribution: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
+				});
+			} else {
+				this.tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+					maxZoom: 19,
+					attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				});
+			}
 
+			
 
-		for (var i = 0; i < jsonData.markers.length; i++) {
-			var jsonMarker = jsonData.markers[i];
-			const svgIcon = L.divIcon(getSuggestoIconOptions(jsonMarker.type, jsonMarker.value, jsonMarker.size));
-			const aMarker = L.marker(jsonMarker.latlng, { icon: svgIcon }).addTo(this.lmap).bindPopup(jsonMarker.html, { maxWidth: "auto" });
-			this.mlist.push(aMarker);
-		}
+			this.tileLayer.addTo(this.lmap);
 
-		for (var i = 0; i < jsonData.layers.length; i++) {
-			var jsonLayer = jsonData.layers[i];
-			if (jsonLayer.value !== '') {
-				if (jsonLayer.type == 'gpxurl') {
-					omnivore.gpx(jsonLayer.value).addTo(this.lmap);
-				} else if (jsonLayer.type == 'gpx') {
-					omnivore.gpx.parse(jsonLayer.value).addTo(this.lmap);
-				} else if (jsonLayer.type == 'kmlurl') {
-					omnivore.kml(jsonLayer.value).addTo(this.lmap);
-				} else if (jsonLayer.type == 'kml') {
-					omnivore.kml.parse(jsonLayer.value).addTo(this.lmap);
-				} else if (jsonLayer.type == 'geojsonurl') {
-					omnivore.geojson(jsonLayer.value).addTo(this.lmap);
-				} else if (jsonLayer.type == 'geojson') {
-					//omnivore.geojson.parse(jsonLayer.value).addTo(this.lmap);
+			
+	
+			for (var i = 0; i < jsonData.markers.length; i++) {
+				var jsonMarker = jsonData.markers[i];
+				const svgIcon = L.divIcon(getSuggestoIconOptions(jsonMarker.type, jsonMarker.value, jsonMarker.size));
+				const aMarker = L.marker(jsonMarker.latlng, { icon: svgIcon }).addTo(this.lmap).bindPopup(jsonMarker.html, { maxWidth: "auto" });
+				this.mlist.push(aMarker);
+			}
+	
+			for (var i = 0; i < jsonData.layers.length; i++) {
+				var jsonLayer = jsonData.layers[i];
+				if (jsonLayer.value !== '') {
+					if (jsonLayer.type == 'gpxurl') {
+						omnivore.gpx(jsonLayer.value).addTo(this.lmap);
+					} else if (jsonLayer.type == 'gpx') {
+						omnivore.gpx.parse(jsonLayer.value).addTo(this.lmap);
+					} else if (jsonLayer.type == 'kmlurl') {
+						omnivore.kml(jsonLayer.value).addTo(this.lmap);
+					} else if (jsonLayer.type == 'kml') {
+						omnivore.kml.parse(jsonLayer.value).addTo(this.lmap);
+					} else if (jsonLayer.type == 'geojsonurl') {
+						omnivore.geojson(jsonLayer.value).addTo(this.lmap);
+					} else if (jsonLayer.type == 'geojson') {
+						//omnivore.geojson.parse(jsonLayer.value).addTo(this.lmap);
+					}
 				}
 			}
-		}
-
-		/*var baseLayers = {
-			"Open Street Map": SuggestoMapLayers[0],
-			"Esri WorldImagery": SuggestoMapLayers[1],
-			"Thunderforest OpenCycleMap (a pagamento)": SuggestoMapLayers[2],
-			"Thunderforest Atlas (a pagamento)": SuggestoMapLayers[3],
-		};
 	
-		L.control.layers(baseLayers).addTo(this.lmap);
-*/
-
-		this.lmap.fitBounds(this.getMarkersLatLngArray());
-
-	},
-	getMarkersLatLngArray() {
-		var ar = [];
-		var that = this;
-		for (var i = 0; i < that.mlist.length; i++) {
-			ar.push(that.mlist[i]._latlng);
+			/*var baseLayers = {
+				"Open Street Map": SuggestoMapLayers[0],
+				"Esri WorldImagery": SuggestoMapLayers[1],
+				"Thunderforest OpenCycleMap (a pagamento)": SuggestoMapLayers[2],
+				"Thunderforest Atlas (a pagamento)": SuggestoMapLayers[3],
+			};
+		
+			L.control.layers(baseLayers).addTo(this.lmap);
+	*/
+	
+			this.lmap.fitBounds(this.getMarkersLatLngArray());
+	
+		},
+		getMarkersLatLngArray() {
+			var ar = [];
+			var that = this;
+			for (var i = 0; i < that.mlist.length; i++) {
+				ar.push(that.mlist[i]._latlng);
+			}
+			return ar;
 		}
-		return ar;
 	}
 }
+
 
 
 
