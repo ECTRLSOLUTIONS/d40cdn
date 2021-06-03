364b65ca21eb459eca4a618b447e0fcab7d417fe
Nicola Spadari <spadari.nicola@gmail.com>
2021-06-03 17:45:33 +0200

    Define global vue instances with dynamic names

--- components/initVueADT.js
+++ components/initVueADT.js
@@ -1,10 +1,7 @@
 function initVueADT(portletId, namespace = "", filterConfig, mixins = []) {
-    console.log("portletId is " + portletId);
-    console.log("namespace is " + namespace);
-    console.log("filterConfig is ", filterConfig);
-    console.log("mixins are ", mixins);
+    var instanceName = "vue_" + portletId.split("INSTANCE_")[1] + "app";
 
-    new Vue({
+    window[instanceName] = new Vue({
         el: portletId,
         mixins: mixins,
         router: new VueRouter({
@@ -18,10 +15,7 @@ function initVueADT(portletId, namespace = "", filterConfig, mixins = []) {
             namespace: namespace,
         },
         created() {
-            console.log("Vue ADT " + this.namespace + " created");
-        },
-        mounted() {
-            console.log("Vue ADT " + this.namespace + " created");
+            console.log("Vue ADT instance created. Access it using window." + instanceName);
         },
     });
 }
