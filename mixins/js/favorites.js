a1110b74288f0e27e4dfc2d90beadffd52478f4e
Nicola Spadari <spadari.nicola@gmail.com>
2021-06-18 10:04:39 +0200

    Add localStorage favorite system

---
+++ mixins/js/favorites.js
@@ -0,0 +1,53 @@
+var d40_favorites = {
+    data: {
+        favorites: [],
+    },
+    mounted() {
+        if (Liferay.ThemeDisplay.isSignedIn()) {
+            this.getFavorites();
+        }
+    },
+    methods: {
+        getFavorites: function () {
+            if (localStorage.getItem("favorites") !== null) {
+                this.favorites = JSON.parse(localStorage.getItem("favorites"));
+            } else {
+                this.favorites = [];
+            }
+        },
+        setFavorite: function (item) {
+            this.getFavorites();
+
+            var newFavorite = {
+                id: item.id,
+                contentJSON: item.contentJSON,
+            };
+
+            this.favorites.push(newFavorite);
+            this.saveFavorites();
+        },
+        removeFavorite: function (item) {
+            this.favorites = this.favorites.filter((obj) => obj.id !== item.id);
+            this.saveFavorites();
+        },
+        isFavorite: function (itemId) {
+            if (this.favorites.some((e) => e.id == itemId)) {
+                return true;
+            }
+
+            return false;
+        },
+        saveFavorites: function () {
+            localStorage.setItem("favorites", JSON.stringify(this.favorites));
+            console.log("favorites updated");
+        },
+        resetAllFavorites: function () {
+            if (confirm("delete all favorites?")) {
+                localStorage.removeItem("favorites");
+                console.log("removed all favorites");
+
+                this.getFavorites();
+            }
+        },
+    },
+};
