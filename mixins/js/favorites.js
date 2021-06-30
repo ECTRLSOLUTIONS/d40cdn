1628c8a612237e9176e00a84aff834a71484ca90
Nicola Spadari <spadari.nicola@gmail.com>
2021-06-30 10:27:09 +0200

    Favorites don't require to be signed in

--- mixins/js/favorites.js
+++ mixins/js/favorites.js
@@ -3,58 +3,50 @@ var d40_favorites = {
         favorites: [],
     },
     mounted() {
-        if (Liferay.ThemeDisplay.isSignedIn()) {
-            this.getFavorites();
-        }
+        this.getFavorites();
     },
     methods: {
-        getFavorites: function () {
+        getFavorites() {
             if (localStorage.getItem("favorites") !== null) {
                 this.favorites = JSON.parse(localStorage.getItem("favorites"));
             } else {
                 this.favorites = [];
             }
         },
-        setFavorite: function (item) {
-            if (Liferay.ThemeDisplay.isSignedIn()) {
-                this.getFavorites();
+        setFavorite(item) {
+            this.getFavorites();
 
-                var newFavorite = {
-                    id: item.id,
-                    contentJSON: {
-                        title: item.contentJSON.title,
-                        previewDescription: item.contentJSON.previewDescription,
-                        description: item.contentJSON.description,
-                        geoRef: item.contentJSON.geoRef,
-                        previewPicture: item.contentJSON.previewPicture,
-                        viewUrl: item.contentJSON.viewUrl,
-                    },
-                };
+            var newFavorite = {
+                id: item.id,
+                contentJSON: {
+                    title: item.contentJSON.title,
+                    previewDescription: item.contentJSON.previewDescription,
+                    description: item.contentJSON.description,
+                    geoRef: item.contentJSON.geoRef,
+                    previewPicture: item.contentJSON.previewPicture,
+                    viewUrl: item.contentJSON.viewUrl,
+                },
+            };
 
-                this.favorites.push(newFavorite);
-                this.saveFavorites();
-            }
+            this.favorites.push(newFavorite);
+            this.saveFavorites();
         },
-        removeFavorite: function (item) {
-            if (Liferay.ThemeDisplay.isSignedIn()) {
-                this.favorites = this.favorites.filter((obj) => obj.id !== item.id);
-                this.saveFavorites();
-            }
+        removeFavorite(item) {
+            this.favorites = this.favorites.filter((obj) => obj.id !== item.id);
+            this.saveFavorites();
         },
-        isFavorite: function (itemId) {
+        isFavorite(itemId) {
             if (this.favorites.some((e) => e.id == itemId)) {
                 return true;
             }
 
             return false;
         },
-        saveFavorites: function () {
-            if (Liferay.ThemeDisplay.isSignedIn()) {
-                localStorage.setItem("favorites", JSON.stringify(this.favorites));
-                console.log("favorites updated");
-            }
+        saveFavorites() {
+            localStorage.setItem("favorites", JSON.stringify(this.favorites));
+            console.log("favorites updated");
         },
-        resetAllFavorites: function () {
+        resetAllFavorites() {
             if (confirm("delete all favorites?")) {
                 localStorage.removeItem("favorites");
                 console.log("removed all favorites");
