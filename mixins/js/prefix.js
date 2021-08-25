50d830c9d7a6802282874fc4e11c7a40a16f5c82
Nicola Spadari <spadari.nicola@gmail.com>
2021-08-25 09:05:09 +0200

    Empty image placeholder fallback

--- mixins/js/prefix.js
+++ mixins/js/prefix.js
@@ -5,13 +5,19 @@ var d40_crop = {
                 imgCdn = "https://d28r45jypu6nt9.cloudfront.net/o/d40/img/",
                 siteUrl = window.location.origin.replace("://", ".");
 
-            if (imgCdn !== "") {
+            if (image == "") {
+                if (h == "") {
+                    h = w;
+                }
+
+                image = `https://via.placeholder.com/${w}x${h}?text=Anteprima`;
+            } else {
                 if (image.startsWith("http") || image == "/documentsundefined") {
                     if (image == "/documentsundefined") {
-                        image = "https://via.placeholder.com/450x300?text=Anteprima";
+                        image = `https://via.placeholder.com/${w}x${h}?text=Anteprima`;
                     }
                 } else {
-                    if (typeof h === "string") {
+                    if (h == "") {
                         prefix = imgCdn + "w_" + w + "/" + siteUrl;
                     } else {
                         prefix = imgCdn + "w_" + w + ",h_" + h + "/" + siteUrl;
