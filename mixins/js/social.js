67f49043fb7b540b8d363283ec1e250f8428de38
Nicola Spadari <spadari.nicola@gmail.com>
2021-08-31 14:03:34 +0200

    Add instagram mixin

---
+++ mixins/js/social.js
@@ -0,0 +1,31 @@
+var d40_social = {
+    data: {
+        socialEndPoint: "https://online.d40.it/web/suggesto-socialfeed/",
+        socialParams: "?p_p_id=Configurable&p_p_lifecycle=2&p_p_resource_id=json",
+        posts: [],
+    },
+    methods: {
+        getPosts(siteName) {
+            var that = this;
+
+            axios
+                .get(this.socialEndPoint + siteName + this.socialParams)
+                .then((res) => {
+                    console.log("Posts: ", res);
+
+                    that.posts = res.data.docs;
+                })
+                .then(() => {
+                    if (typeof that.initLazyLoad === "function") {
+                        that.initLazyLoad();
+                    }
+                })
+                .catch((err) => {
+                    console.error("Error getting posts: ", err);
+                })
+                .finally(() => {
+                    that.loading = false;
+                });
+        },
+    },
+};
