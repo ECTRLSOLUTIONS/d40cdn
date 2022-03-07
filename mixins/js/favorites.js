4c168c376a56feed1d7b74190758c7b72fc609ce
NicolaSpadari <spadari.nicola@gmail.com>
2022-03-07 16:19:36 +0100

    Save categories for yucca content too

--- mixins/js/favorites.js
+++ mixins/js/favorites.js
@@ -1,84 +1,87 @@
 var d40_favorites = {
-    data: {
-        favorites: [],
-    },
-    mounted() {
-        this.getFavorites();
-    },
-    methods: {
-        getFavorites() {
-            if (localStorage.getItem("favorites") !== null) {
-                this.favorites = JSON.parse(localStorage.getItem("favorites"));
-            } else {
-                this.favorites = [];
-            }
-        },
-        setFavorite(item) {
-            this.getFavorites();
+	data: {
+		favorites: [],
+	},
+	mounted() {
+		this.getFavorites();
+	},
+	methods: {
+		getFavorites() {
+			if (localStorage.getItem("favorites") !== null) {
+				this.favorites = JSON.parse(localStorage.getItem("favorites"));
+			} else {
+				this.favorites = [];
+			}
+		},
+		setFavorite(item) {
+			this.getFavorites();
 
-            var newFavorite = {
-                id: item.id,
-                contentJSON: {
-                    title: item.contentJSON.title,
-                    previewDescription: item.contentJSON.previewDescription,
-                    description: item.contentJSON.description,
-                    geoRef: item.contentJSON.geoRef,
-                    previewPicture: item.contentJSON.previewPicture,
-                    viewUrl: item.contentJSON.viewUrl,
-                },
-            };
+			var newFavorite = {
+				id: item.id,
+				contentJSON: {
+					title: item.contentJSON.title,
+					previewDescription: item.contentJSON.previewDescription,
+					description: item.contentJSON.description,
+					geoRef: item.contentJSON.geoRef,
+					previewPicture: item.contentJSON.previewPicture,
+					viewUrl: item.contentJSON.viewUrl,
+					type: "lfr",
+				},
+			};
 
-            this.favorites.push(newFavorite);
-            this.saveFavorites();
-        },
-        setYuccaFavorite(item) {
-            this.getFavorites();
+			this.favorites.push(newFavorite);
+			this.saveFavorites();
+		},
+		setYuccaFavorite(item) {
+			this.getFavorites();
 
-            var newFavorite = {
-                id: item.nativeId,
-                contentJSON: {
-                    title: item.denominazione,
-                    previewDescription: "",
-                    description: "",
-                    geoRef: ["", ""],
-                    previewPicture: "https://via.placeholder.com/500",
-                    viewUrl: item.www,
-                },
-            };
+			var newFavorite = {
+				id: item.nativeId,
+				contentJSON: {
+					title: item.denominazione,
+					previewDescription: "",
+					assetCategoryIds: item.assetCategoryIds,
+					description: "",
+					geoRef: ["", ""],
+					previewPicture: "https://via.placeholder.com/500",
+					viewUrl: item.www,
+					type: "yucca",
+				},
+			};
 
-            this.favorites.push(newFavorite);
-            this.saveFavorites();
-        },
-        removeFavorite(item) {
-            this.favorites = this.favorites.filter((obj) => obj.id !== item.id);
-            this.saveFavorites();
-        },
-        removeYuccaFavorite(item) {
-            this.favorites = this.favorites.filter((obj) => obj.id !== item.nativeId);
-            this.saveFavorites();
-        },
-        isFavorite(itemId) {
-            if (this.favorites.some((e) => e.id == itemId)) {
-                return true;
-            }
+			this.favorites.push(newFavorite);
+			this.saveFavorites();
+		},
+		removeFavorite(item) {
+			this.favorites = this.favorites.filter((obj) => obj.id !== item.id);
+			this.saveFavorites();
+		},
+		removeYuccaFavorite(item) {
+			this.favorites = this.favorites.filter((obj) => obj.id !== item.nativeId);
+			this.saveFavorites();
+		},
+		isFavorite(itemId) {
+			if (this.favorites.some((e) => e.id == itemId)) {
+				return true;
+			}
 
-            return false;
-        },
-        saveFavorites() {
-            localStorage.setItem("favorites", JSON.stringify(this.favorites));
-            if (this.filterConfig?.debugMode || this.debugMode) {
-                console.log("favorites updated");
-            }
-        },
-        resetAllFavorites() {
-            if (confirm("delete all favorites?")) {
-                localStorage.removeItem("favorites");
-                if (this.filterConfig?.debugMode || this.debugMode) {
-                    console.log("removed all favorites");
-                }
+			return false;
+		},
+		saveFavorites() {
+			localStorage.setItem("favorites", JSON.stringify(this.favorites));
+			if (this.filterConfig?.debugMode || this.debugMode) {
+				console.log("favorites updated");
+			}
+		},
+		resetAllFavorites() {
+			if (confirm("delete all favorites?")) {
+				localStorage.removeItem("favorites");
+				if (this.filterConfig?.debugMode || this.debugMode) {
+					console.log("removed all favorites");
+				}
 
-                this.getFavorites();
-            }
-        },
-    },
+				this.getFavorites();
+			}
+		},
+	},
 };
